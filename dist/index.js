912587605e40d9678f93840f8f5a066aac5b9dc7
BraisK <braiscasares26@gmail.com>
2024-10-29 14:55:06 +0100

    29/10/24

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('hola');
+let edad = 20;
+let nombre = 'Pedro';
+const estado = true;
+let numeros = [1, 4, 5];
+numeros = [6, 7];
+let nombres = ['Juan', 'Pedro', -13];
+let valor = 'Hola';
+valor = 6;
+function sumar(a, b) {
+    return a + b;
+}
+function error(msg) {
+    throw new Error(msg);
+}
+let nulo = null;
+let indefinida = undefined;
+let identificador = 'xx';
+identificador = 66;
+let state = 'Activo';
+state = 'Inactivo';
+state = 'Pendiente';
+state = 'cosa';
+console.log(edad);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,72 @@
-console.log('hola')
+let edad:number = 20
+
+let nombre = 'Pedro'
+
+const estado:boolean = true
+
+let numeros:number[] = [1,4,5]
+
+numeros = [6,7]
+
+let nombres:[string,string,number] = ['Juan', 'Pedro', -13]
+
+
+let valor:any = 'Hola'
+valor=6
+
+function sumar(a:number,b:number):number{
+    return a+b
+}
+
+function error(msg:string):never{
+    throw new Error(msg)
+}
+
+let nulo:null = null
+let indefinida:undefined = undefined
+
+let identificador: string | number = 'xx'
+identificador = 66
+
+let state:'Activo' | 'Inactivo' | 'Pendiente' = 'Activo'
+state = 'Inactivo'
+state = 'Pendiente'
+//state = 'cosa'
+
+console.log(edad) 
+
+const producto:{nombre:string,precio:number,stock:boolean} = {
+    nombre: 'ordenador',
+    precio: 2400,
+    stock: true
+}
+
+const jugadores: {nombre:string,ptos:number}[]
+= [
+    {nombre:'Pepe', ptos:33},
+    {nombre:'Ana', ptos:22},
+    {nombre:'Luis', ptos:44}
+]
+
+interface Direccion{
+    street: string
+    number: number
+}
+
+interface Producto{
+    name: string
+    price: number
+    stock: boolean
+    direccion: Direccion
+}
+
+const product:Producto = {
+    name: 'ordenador',
+    price: 2400,
+    stock: true,
+    direccion:{
+        street:'hispanidad',
+        number:33        
+    }
+
+}
